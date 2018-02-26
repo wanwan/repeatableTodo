9cd023947509b05dbfddf1b41e998b53b2bbb79f
Ayumu WAKABAYASHI <waka@zaregoto.org>
2018-02-27 00:12:45 +0900

    modify task view from listview to cardview

--- app/src/main/java/org/zaregoto/apl/repeatabletodo/EditTaskListActivity.java
+++ app/src/main/java/org/zaregoto/apl/repeatabletodo/EditTaskListActivity.java
@@ -6,6 +6,9 @@ import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,7 +25,7 @@ import java.io.IOException;
 public class EditTaskListActivity extends Activity implements EditTaskDialogFragment.EditTaskCallback {
 
     //private TaskList mTaskList;
-    private ArrayAdapter<Task> adapter = null;
+    private TaskListAdapter adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,51 +47,57 @@ public class EditTaskListActivity extends Activity implements EditTaskDialogFrag
         });
 
         TaskList tasklist = ((MainApplication)getApplication()).getTaskList();
-        ListView lv = findViewById(R.id.task_list);
-        adapter = new TaskListAdapter(this, 0, tasklist.getTasks());
-        if (null != lv) {
-            lv.setAdapter(adapter);
-            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+        RecyclerView rv = findViewById(R.id.task_list);
 
-                    Task task = adapter.getItem(pos);
-                    EditTaskDialogFragment dialog = EditTaskDialogFragment.newInstance(task, EditTaskDialogFragment.EDIT_TASK_DIALOG_MODE.EDIT_TASK);
+        rv.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        rv.setLayoutManager(llm);
 
-                    FragmentManager fm = EditTaskListActivity.this.getFragmentManager();
-                    dialog.show(fm, "");
-                }
-            });
-            lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-                @Override
-                public boolean onItemLongClick(AdapterView<?> adapterView, View view, int pos, long id) {
-
-                    final Task task = adapter.getItem(pos);
-
-                    AlertDialog.Builder alertBulider = new AlertDialog.Builder(EditTaskListActivity.this);
-                    AlertDialog dialog = alertBulider
-                            .setTitle(R.string.remove_task_dialog_title)
-                            .setMessage(R.string.remove_task_dialog_message)
-                            .setPositiveButton(R.string.remove_task_dialog_ok_btn, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    MainApplication ma = (MainApplication) EditTaskListActivity.this.getApplication();
-                                    ma.getTaskList().removeTask(task);
-
-                                    adapter.notifyDataSetChanged();
-                                }
-                            })
-                            .setNegativeButton(R.string.remove_task_dialog_cancel_btn, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    // do nothing
-                                }
-                            }).create();
-
-                    dialog.show();
-                    return true;
-                }
-            });
+        adapter = new TaskListAdapter(tasklist.getTasks());
+        if (null != rv) {
+            rv.setAdapter(adapter);
+//            rv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+//
+//                    Task task = adapter.getItem(pos);
+//                    EditTaskDialogFragment dialog = EditTaskDialogFragment.newInstance(task, EditTaskDialogFragment.EDIT_TASK_DIALOG_MODE.EDIT_TASK);
+//
+//                    FragmentManager fm = EditTaskListActivity.this.getFragmentManager();
+//                    dialog.show(fm, "");
+//                }
+//            });
+//            rv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//                @Override
+//                public boolean onItemLongClick(AdapterView<?> adapterView, View view, int pos, long id) {
+//
+//                    final Task task = adapter.getItem(pos);
+//
+//                    AlertDialog.Builder alertBulider = new AlertDialog.Builder(EditTaskListActivity.this);
+//                    AlertDialog dialog = alertBulider
+//                            .setTitle(R.string.remove_task_dialog_title)
+//                            .setMessage(R.string.remove_task_dialog_message)
+//                            .setPositiveButton(R.string.remove_task_dialog_ok_btn, new DialogInterface.OnClickListener() {
+//                                @Override
+//                                public void onClick(DialogInterface dialogInterface, int i) {
+//                                    MainApplication ma = (MainApplication) EditTaskListActivity.this.getApplication();
+//                                    ma.getTaskList().removeTask(task);
+//
+//                                    adapter.notifyDataSetChanged();
+//                                }
+//                            })
+//                            .setNegativeButton(R.string.remove_task_dialog_cancel_btn, new DialogInterface.OnClickListener() {
+//                                @Override
+//                                public void onClick(DialogInterface dialogInterface, int i) {
+//                                    // do nothing
+//                                }
+//                            }).create();
+//
+//                    dialog.show();
+//                    return true;
+//                }
+//            });
         }
     }
 
--- app/src/main/java/org/zaregoto/apl/repeatabletodo/TaskListAdapter.java
+++ app/src/main/java/org/zaregoto/apl/repeatabletodo/TaskListAdapter.java
@@ -1,6 +1,7 @@
 package org.zaregoto.apl.repeatabletodo;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,38 +11,71 @@ import org.zaregoto.apl.repeatabletodo.model.Task;
 
 import java.util.List;
 
-public class TaskListAdapter extends ArrayAdapter<Task> {
+public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.ContactViewHolder> {
 
-    LayoutInflater mInflater;
+    private LayoutInflater mInflater;
+    private List<Task> contactList;
 
-    public TaskListAdapter(Context context, int resource, List<Task> objects) {
-        super(context, resource, objects);
-        mInflater = LayoutInflater.from(context);
+    public TaskListAdapter(List<Task> contactList) {
+        this.contactList = contactList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(ContactViewHolder contactViewHolder, int i) {
+        Task task = contactList.get(i);
+        contactViewHolder.taskName.setText(task.getName());
+        contactViewHolder.taskDetail.setText(task.getDetail());
+    }
+
+    @Override
+    public ContactViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.adapter_task_item, viewGroup, false);
+
+        return new ContactViewHolder(itemView);
     }
 
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//
+//        Task task;
+//        TextView tv;
+//
+//        if (null == convertView) {
+//            convertView = mInflater.inflate(R.layout.adapter_task_item, parent, false);
+//        }
+//
+//        task = getItem(position);
+//        if (null != task) {
+//            tv = convertView.findViewById(R.id.taskname);
+//            if (null != tv) {
+//                tv.setText(task.getName());
+//            }
+//            tv = convertView.findViewById(R.id.taskdetail);
+//            if (null != tv) {
+//                tv.setText(task.getDetail());
+//            }
+//        }
+//
+//        return convertView;
+//    }
 
-        Task task;
-        TextView tv;
 
-        if (null == convertView) {
-            convertView = mInflater.inflate(R.layout.adapter_task_item, parent, false);
+    public static class ContactViewHolder extends RecyclerView.ViewHolder {
+        protected TextView taskName;
+        protected TextView taskDetail;
+
+        public ContactViewHolder(View v) {
+            super(v);
+            taskName =  (TextView) v.findViewById(R.id.taskname);
+            taskDetail = (TextView)  v.findViewById(R.id.taskdetail);
         }
-
-        task = getItem(position);
-        if (null != task) {
-            tv = convertView.findViewById(R.id.taskname);
-            if (null != tv) {
-                tv.setText(task.getName());
-            }
-            tv = convertView.findViewById(R.id.taskdetail);
-            if (null != tv) {
-                tv.setText(task.getDetail());
-            }
-        }
-
-        return convertView;
     }
 }
