d5ecf905e6b8f2251b5c8ba12f5b1f5229b9774e
Ayumu WAKABAYASHI <waka@zaregoto.org>
2018-03-07 23:28:57 +0900

    add configuration skelton

--- app/src/main/java/org/zaregoto/apl/repeatabletodo/ConfigurationActivity.java
+++ app/src/main/java/org/zaregoto/apl/repeatabletodo/ConfigurationActivity.java
@@ -1,6 +1,13 @@
 package org.zaregoto.apl.repeatabletodo;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ConfigurationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configuration);
+    }
 }
