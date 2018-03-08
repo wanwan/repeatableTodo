9eabf0fc9b8834989d5e1910fa38e94f4aadd5f1
Ayumu WAKABAYASHI <waka@zaregoto.org>
2018-03-08 20:20:59 +0900

    add timepicker to configuration activitiy

--- app/src/main/java/org/zaregoto/apl/repeatabletodo/ConfigurationActivity.java
+++ app/src/main/java/org/zaregoto/apl/repeatabletodo/ConfigurationActivity.java
@@ -1,7 +1,12 @@
 package org.zaregoto.apl.repeatabletodo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class ConfigurationActivity extends Activity {
 
@@ -9,5 +14,28 @@ public class ConfigurationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.configuration);
+
+        TextView tv = findViewById(R.id.configuration_time_select);
+        if (null != tv) {
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //TimePickerDialog.Builder builder = new TimePickerDialog.Builder();
+                    //AlertDialog dialog = builder.create();
+                    TimePickerDialog dialog = new TimePickerDialog(
+                            ConfigurationActivity.this,
+                            new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                                }
+                            },
+                            0,
+                            0,
+                            true
+                    );
+                    dialog.show();
+                }
+            });
+        }
     }
 }
