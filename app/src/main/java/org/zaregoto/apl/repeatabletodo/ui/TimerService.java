4d0759f2285fd1a65ce6ed3a361c79037df727db
Ayumu WAKABAYASHI <waka@zaregoto.org>
2018-03-22 21:55:54 +0900

    add timer service skelton

---
+++ app/src/main/java/org/zaregoto/apl/repeatabletodo/ui/TimerService.java
@@ -0,0 +1,14 @@
+package org.zaregoto.apl.repeatabletodo.ui;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TimerService extends Service {
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
