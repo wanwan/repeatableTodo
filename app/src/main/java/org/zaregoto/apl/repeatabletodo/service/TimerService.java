e714533872e16287a50867f776299f233502f104
Ayumu WAKABAYASHI <waka@zaregoto.org>
2018-04-01 23:17:23 +0900

    add timer

--- app/src/main/java/org/zaregoto/apl/repeatabletodo/service/TimerService.java
+++ app/src/main/java/org/zaregoto/apl/repeatabletodo/service/TimerService.java
@@ -4,12 +4,36 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TimerService extends Service {
 
     private static final String TAG = "TimerService";
+    private long TIMER_DELAY_MS = (1*60*1000);
+    private long TIMER_PERIOD_MS = (1*60*1000);
+
+    private static Timer mTimer;
 
     @Override
     public IBinder onBind(Intent intent) {
+
+        TodoMakeTimerTask timerTask;
+
+        if (null == mTimer) {
+            //タイマーの初期化処理
+            timerTask = new TodoMakeTimerTask();
+            mTimer = new Timer(true);
+            mTimer.schedule( timerTask, TIMER_DELAY_MS, TIMER_PERIOD_MS);
+        }
+
         return null;
+    }
+
+    private class TodoMakeTimerTask extends TimerTask {
+        @Override
+        public void run() {
+
+        }
     }
 }
